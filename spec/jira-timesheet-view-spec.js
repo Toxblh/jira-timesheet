edd3e624e7b83ea8c3085c7a2d9f309adda9f5b0
Anton Palgunov <toxblh@gmail.com>
2016-09-01 15:45:06 +0300

    init

---
+++ spec/jira-timesheet-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JiraTimesheetView from '../lib/jira-timesheet-view';
+
+describe('JiraTimesheetView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
